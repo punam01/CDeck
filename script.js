9981e5ea32c7ba987cfa4fff8bb580e85b573f13
Punam Kumavat <punamkumavat01@gmail.com>
2021-09-06 20:46:30 +0530

    Added ExamDeck Section >>QP

--- script.js
+++ script.js
@@ -16,3 +16,46 @@ document.querySelector('.scroll-btn').addEventListener("click",
         document.querySelector('html').style.scrollBehavior="unset";
     },1000)
 });  
+
+
+let questPaper=document.querySelector('.quest');
+let notes=document.querySelector('.notes');
+let paperDiv=document.querySelector('.e-paper');
+let notesDiv=document.querySelector('.e-notes');
+
+questPaper.addEventListener('click',()=>{
+    if(paperDiv.style.display==="none"){
+        notesDiv.style.display="none";
+        paperDiv.style.display="block";
+    }
+    else{
+        paperDiv.style.display="none";
+    }
+})
+notes.addEventListener('click',()=>{
+    if(notesDiv.style.display==="none"){
+        paperDiv.style.display="none";
+        notesDiv.style.display="block";
+    }
+    else{
+        notesDiv.style.display="none";
+    }
+})
+
+
+let labelBtn=document.querySelectorAll('.head');
+
+function expand(val){
+    var x = document.querySelector("."+val);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+
+
+
+
